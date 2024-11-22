76973be9effbd5e13908e595ec548f6aaf3d8b41
angus-chanan11 <angus.chan@oocl.com>
2024-11-22 11:59:35 +0800

    [ParkingLot] feat: return car when fetch with ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParkingLotTest {
@@ -15,5 +16,17 @@ class ParkingLotTest {
         Ticket ticket = parkingLot.park(car);
         // Then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_the_car_when_fetch_given_a_ticket(){
+        // Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+        // When
+        Car fetchedCar = parkingLot.fetch(ticket);
+        // Then
+        assertEquals(car, fetchedCar);
     }
 }
