4c7d98e0a9cab451a393b2bee0fdee445daf89a7
angus-chanan11 <angus.chan@oocl.com>
2024-11-23 13:37:28 +0800

    [ParkingLot] feat: return correct car when fetch twice

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartParkingBoyTest {
+    public static final int PARKING_LOT_CAPACITY = 10;
+
     @Test
     void should_return_ticket_and_park_to_first_lot_when_park_given_a_car_and_2_parking_lot_where_both_have_equal_amount_of_car(){
         // Given
@@ -40,5 +42,27 @@ public class SmartParkingBoyTest {
         assertNotNull(ticket);
         assertFalse(firstParkingLot.getCurrentlyParkedCars().contains(car));
         assertTrue(secondParkingLot.getCurrentlyParkedCars().contains(car));
+    }
+
+    @Test
+    void should_return_correct_car_when_fetch_twice_given_2_ticket_and_2_parking_lot(){
+        // Given
+        ParkingLot firstParkingLot = new ParkingLot();
+        for (int i = 0; i < PARKING_LOT_CAPACITY - 1; i++) {
+            firstParkingLot.park(new Car());
+        }
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        Ticket firstTicket = smartParkingBoy.park(firstCar);
+        Ticket secondTicket = smartParkingBoy.park(secondCar);
+        // When
+        Car firstFetchedCar = smartParkingBoy.fetch(firstTicket);
+        Car secondFetchedCar = smartParkingBoy.fetch(secondTicket);
+        // Then
+        assertEquals(firstCar, firstFetchedCar);
+        assertEquals(secondCar, secondFetchedCar);
     }
 }
