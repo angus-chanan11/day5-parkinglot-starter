7521c9acb1dab6ab2cf8d0c760481fc7f7a786a2
angus-chanan11 <angus.chan@oocl.com>
2024-11-23 13:27:35 +0800

    [ParkingLot] feat: return ticket and park to second lot when first lot have more cars

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -24,4 +24,21 @@ public class SmartParkingBoyTest {
         assertTrue(firstParkingLot.getCurrentlyParkedCars().contains(car));
         assertFalse(secondParkingLot.getCurrentlyParkedCars().contains(car));
     }
+
+    @Test
+    void should_return_ticket_and_park_to_second_lot_when_park_given_a_car_and_2_parking_lot_where_first_have_more_car_parked(){
+        // Given
+        ParkingLot firstParkingLot = new ParkingLot();
+        firstParkingLot.park(new Car());
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        // When
+        Ticket ticket = smartParkingBoy.park(car);
+        // Then
+        assertNotNull(ticket);
+        assertFalse(firstParkingLot.getCurrentlyParkedCars().contains(car));
+        assertTrue(secondParkingLot.getCurrentlyParkedCars().contains(car));
+    }
 }
